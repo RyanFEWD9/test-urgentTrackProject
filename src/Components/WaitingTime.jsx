c328e067adfeb2a29a69b64fb6a330812a3dfbfd
ac1002 <13202081@life.hkbu.edu.hk>
2023-12-26 23:28:24 +0800

    App.js and WaitingTime.js updated by Abbie on 20231226
    1. New packages added : cors, express, nodemon
    2. App.js deleted red p text for latitude and longtitude
    3. App.js updated h1 as 香港公立醫院
    4. WaitingTime.js updated with new API

--- src/Components/WaitingTime.jsx
+++ src/Components/WaitingTime.jsx
@@ -1,19 +1,18 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 function WaitingTime(props) {
   const [isFetching, setIsFetching] = useState(false);
   const [characters, setCharacters] = useState([]); // array
+  const [latestTime, setLatestTime] = useState("");
 
   useEffect(() => {
     const getData = async () => {
       try {
         setIsFetching(true);
-        const res = await fetch(props.API);
-        const { features } = await res.json();
-        setCharacters(features);
-        // console.log(features);
+        const res = await fetch(`${props.API}`);
+        const { waitTime, updateTime } = await res.json();
+        setCharacters(waitTime);
+        setLatestTime(updateTime);
       } catch (err) {
         console.log(err);
       } finally {
@@ -25,18 +24,16 @@ function WaitingTime(props) {
 
   return (
     <div>
+      <h2>急症室等候時間</h2>
       {isFetching
-        ? "Loading..."
-        : characters.map((features) => (
-            <div key={features.properties.OBJECTID}>
-              <p>{features.properties.hospName_TC}</p>
-              <ul>
-                <li>LATITUDE : {features.properties.LATITUDE}</li>
-                <li>LONGITUDE: {features.properties.LONGITUDE}</li>
-                {/* {console.log(features.properties.JSON_TC)} */}
-              </ul>
+        ? "更新中..."
+        : characters.map(({ hospName, topWait }) => (
+            <div key={hospName} className="wait-Time">
+              <p>{hospName}</p>
+              <p>{topWait}</p>
             </div>
           ))}
+      <p>最後更新時間：{latestTime}</p>
     </div>
   );
 }
