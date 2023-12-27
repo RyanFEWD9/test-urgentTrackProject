5a92701bf4445a09aa84c27dfce894a4d7b7afbc
kelly7y176 <125940090+kelly7y176@users.noreply.github.com>
2023-12-27 11:02:20 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from "./logo.svg";
 import "./App.css";
 import MyLocation from "./Components/MyLocation";
 import WaitingTime from "./Components/WaitingTime";
 import Filter from "./Components/Filter";
 import Distance from "./Components/Distance";
-import Address from "./Components/Address";
+
 import Map from "./Components/Map";
-import Redirection from "./Components/Redirection";
+
+import SearchBar from "./Components/SearchBar";
+import LastUploadTime from "./Components/LastUploadTime";
 
 function App() {
   //Below is A&E waiting time
@@ -20,26 +21,34 @@ function App() {
 
   return (
     <div>
-      <h1>香港公立醫院</h1>
+      <img
+        src="public/HospitalLogoWithName.png"
+        alt="HosipitalLogo"
+        width="200em"
+      />
+      <h1>急症室等候時間</h1>
+      <SearchBar />
       {/* page1 */}
       <MyLocation /> {/*Your Current Latitude and Longtitude in console*/}
       <WaitingTime API={API1} />
       {/*Hospital name, waiting time and last update time : Abbie*/}
       <Filter />
       {/*according to the district*/}
-      <Distance />
-      {/*calc distance function, need to get user gps and Hospital
-      location: Ryan*/}
-      <Address />
-      {/*Onclick function for address and contact*/}
-      {/* page2 */}
+      {/*Redirect to google map with last update time */}
+      <p style={{ color: "red" }}>
+        LATITUDE and LONGITUDE just for our REF, will delete later
+      </p>
+      {/* page1 */}
       <Map />
       {/*need Hospital coordinate and data from <Distance /> and <Adderss />, show
       address, tel and website*/}
-      <Redirection />
-      {/*Redirect to google map with last update time */}
+      <Distance />
+      {/*calc distance function, need to get user gps and Hospital
+      location: Ryan*/}
+      <LastUploadTime API={API1} />
     </div>
   );
 }
 
 export default App;
+
