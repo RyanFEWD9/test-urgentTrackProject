767f5567d7e9ce2113ba4a47d7056ed139cb167f
kelly7y176 <125940090+kelly7y176@users.noreply.github.com>
2024-01-02 05:44:40 +0800

    Add files via upload

--- src/Components/AboutUsPage.jsx
+++ src/Components/AboutUsPage.jsx
@@ -5,19 +5,32 @@ import styles from "../App.module.css";
 function AboutUsPage() {
   return (
     <main>
-    <section>
-      <h3>助你輕鬆搜尋</h3>
-      <h1>附近的24小時開放急症室</h1>
-      <p>
-        同時還能夠了解附近醫院的專科門診時間表和最新的症狀輪候時間。無論您需要緊急處理還是尋找其他醫療服務，這個網站都能幫助您找到最適合您需求的醫療資源。
-      </p>
-      <button>
-        <Link to="/UrgentTrackProject">附近急症室</Link>
-      </button>
-      <button>
-        <Link to="/service">專科服務</Link>
-      </button>
-    </section>
+      <section className={styles["mainWrapper"]}>
+        <section className={styles["introWrapper"]}>
+          <div className={styles["introTextWrapper"]}>
+            <h3>助你輕鬆搜尋</h3>
+            <h1>附近的24小時開放急症室</h1>
+            <p>
+              同時還能夠了解附近醫院的專科門診時間表和最新的症狀輪候時間。無論您需要緊急處理還是尋找其他醫療服務，這個網站都能幫助您找到最適合您需求的醫療資源。
+            </p>
+            <section className={styles["chooseServiceButtonWrapper"]}>
+              <button className={styles["chooseServiceButton"]}>
+                <Link to="/UrgentTrackProject">附近急症室</Link>
+              </button>
+              <button className={styles["chooseServiceButton"]}>
+                <Link to="/service">專科服務</Link>
+              </button>
+            </section>
+          </div>
+
+          <div>
+            <img
+              src="https://img.freepik.com/free-vector/hospital-building-concept-illustration_114360-8440.jpg?w=1380&t=st=1704138563~exp=1704139163~hmac=5a196dd6626a106014470b51be11b5c0527a95fe9940e63d06004d1e802e77d4"
+              className={styles["imgWrapper"]}
+            ></img>
+          </div>
+        </section>
+      </section>
     </main>
   );
 }
