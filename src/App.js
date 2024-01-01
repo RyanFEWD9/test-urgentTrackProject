031ff22067b0beb2dca5813d77561280125f61ec
kelly7y176 <125940090+kelly7y176@users.noreply.github.com>
2024-01-02 05:43:13 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Distance from "./Components/Distance";
 import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import styles from "./App.module.css";
 
 import ServicePage from "./Components/ServicePage";
 import AboutUsPage from "./Components/AboutUsPage";
@@ -10,12 +11,18 @@ function App() {
   const [userLocation, setUserLocation] = useState(null);
   return (
     <Router>
-      <nav>
-        {/* Links to navigate between pages */}
-        <Link to="/about-us">主頁 </Link>
-        <Link to="/UrgentTrackProject">附近急症室 </Link>
-        <Link to="/service">專科服務 </Link>
-      </nav>
+      <section className={styles["navBar-logoName"]}>
+        <span>UrgentTrack</span>
+      </section>
+      <div className={styles["navBar-homepage"]}>
+        <nav>
+          {/* Links to navigate between pages */}
+
+          <Link to="/about-us"> 主要頁</Link>
+          <Link to="/UrgentTrackProject"> 急症室</Link>
+          <Link to="/service">專科服務</Link>
+        </nav>
+      </div>
 
       <Routes>
         <Route
