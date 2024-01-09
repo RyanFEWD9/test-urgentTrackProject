8165e6ce954860281c850388fbdd17352a43db51
RyanFEWD9 <ryanli1314@gmail.com>
2024-01-09 11:39:33 +0800

    fixed scroll-to-top button CSS in App.module.css

--- src/Components/ScrollToTop.jsx
+++ src/Components/ScrollToTop.jsx
@@ -4,6 +4,8 @@ import styles from "../App.module.css";
 function ScrollTop() {
   return (
     <div>
+      <div style={{ marginTop: 120, weight: 600 }} />
+
       <ScrollToTop smooth className={styles.smallArrow} />
     </div>
   );
